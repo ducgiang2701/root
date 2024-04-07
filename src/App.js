46e0a58e68565452adcbd0408498a70c52c4103f
Ducgiang0803 <ducgiang21062001@gmail.com>
2024-04-07 15:20:23 +0700

    first code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useLayoutEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
-function App() {
+// import DefaultLayout from './Layouts/DefaultLayout';
+
+// import Homepage from './Pages/Homepage';
+// import Error from './Pages/Error';
+// import Faq from './Pages/Faq';
+
+const Homepage = React.lazy(() => import("./Pages/Homepage"));
+// const Tokenomics = React.lazy(() => import("./Pages/Tokenomics"));
+const App = () => {
+  // let location = useLocation();
+
+  // function useWindowSize() {
+  //   const [size, setSize] = useState([0, 0]);
+  //   useLayoutEffect(() => {
+  //     function updateSize() {
+  //       setSize([window.innerWidth, window.innerHeight]);
+  //     }
+  //     window.addEventListener("resize", updateSize);
+  //     updateSize();
+  //     return () => window.removeEventListener("resize", updateSize);
+  //   }, []);
+  //   return size;
+  // }
+
+  // const [, windowHeight] = useWindowSize();
+
+  // useEffect(() => {
+  //   const style = document.documentElement.style;
+  //   style.setProperty("--height", `${windowHeight}px`);
+  // }, [windowHeight]);
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, [location]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <Routes>
+        <Route index element={<Homepage />} />
+        {/* <Route path="/tokenomics" element={<Tokenomics />} /> */}
+      </Routes>
+    </Suspense>
   );
-}
+};
 
 export default App;
